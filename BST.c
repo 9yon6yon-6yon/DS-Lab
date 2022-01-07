86e69a0921b650d069c5e58958656dbf6091dcf9
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-01-07 14:43:17 +0600

    Binary Search Tree (Completed)

--- BST.c
+++ BST.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdbool.h>
 struct BST
 {
     int data;
@@ -67,7 +67,86 @@ void print_postorder(node *root)
     print_postorder(root->rightchild);
     printf("%d -> ", root->data);
 }
+node *minValueNode(node *root)
+{
+    node *current = root;
+    while (current && current->leftchild != NULL)
+        current = current->leftchild;
 
+    return current;
+}
+node *delete (node *root, int value)
+{
+    if (root == NULL)
+        return root;
+    if (value < root->data)
+        root->leftchild = delete (root->leftchild, value);
+
+    else if (value > root->data)
+        root->rightchild = delete (root->rightchild, value);
+    else
+    {
+
+        if (root->leftchild == NULL)
+        {
+            node *temp = root->rightchild;
+            free(root);
+            return temp;
+        }
+        else if (root->rightchild == NULL)
+        {
+            node *temp = root->leftchild;
+            free(root);
+            return temp;
+        }
+        node *temp = minValueNode(root->rightchild);
+        root->data = temp->data;
+        root->rightchild = delete (root->rightchild, temp->data);
+    }
+    return root;
+}
+
+bool isleaf(node *root)
+{
+    return root->leftchild == NULL && root->rightchild == NULL;
+}
+int height(node *root)
+{
+    if (root == NULL)
+        return -1;
+    if (isleaf(root))
+        return 0;
+    return 1 + max(height(root->leftchild), height(root->rightchild));
+}
+int max(int n1, int n2)
+{
+    return (n1 > n2) ? n1 : n2;
+}
+int size(node *root)
+{
+    if (root == NULL)
+        return 0;
+    if (isleaf(root))
+        return 1;
+    return 1 + size(root->leftchild) + size(root->rightchild);
+}
+int leaves(node *root)
+{
+    if (root == NULL)
+        return 0;
+    if (isleaf(root))
+        return 1;
+    return leaves(root->leftchild) + leaves(root->rightchild);
+}
+bool contains(node *root, int value)
+{
+    if (root == NULL)
+        return false;
+
+    if (root->data == value)
+        return true;
+    return contains(root->leftchild, value) || contains(root->rightchild, value);
+}
 int main()
 {
     int choice, value;
@@ -81,6 +160,12 @@ int main()
             printf("Enter value : ");
             scanf("%d", &value);
             root = insert(root, value);
+        }
+        else if (choice == 2)
+        {
+            printf("Enter value to delete : ");
+            scanf("%d", &value);
+            root = delete (root, value);
         }
         else if (choice == 7)
         {
