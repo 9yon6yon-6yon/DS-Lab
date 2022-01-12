385ccfd6bef5a0f9750bf20224558299f7fb1c81
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-01-12 15:08:36 +0600

    lvl 2 noob doing graph

--- Graph_12_1_22.c
+++ Graph_12_1_22.c
@@ -1,31 +1,29 @@
-/*#include <stdio.h>
+#include <stdio.h>
 
 int main()
 {
     int n, i, j;
     printf("Enter number of Vertices : ");
     scanf("%d", &n);
-    int arr[n][n];
+    int arr[n + 1][n + 1];
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n; j++)
+        for (j = i + 1; j < n; j++)
+        {
+            printf("Is there any edge between %c & %c ? ", 65 + i, 65 + j);
             scanf("%d", &arr[i][j]);
+            arr[j][i] = arr[i][j];
+        }
     }
     printf("Graph :\n");
     for (i = 0; i < n; i++)
     {
         for (j = 0; j < n; j++)
         {
-            printf("%d ", arr[i][j]);
+            printf("%d\t", arr[i][j]);
         }
         printf("\n");
     }
 
-    return 0;
-}*/
-#include <stdio.h>
-
-int main()
-{
     return 0;
 }
