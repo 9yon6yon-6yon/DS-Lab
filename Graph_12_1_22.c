d5e701d4c3c7f2fefe7eabef1c92345ea7ed21b8
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-01-12 14:46:25 +0600

    Noob Graph

---
+++ Graph_12_1_22.c
@@ -0,0 +1,31 @@
+/*#include <stdio.h>
+
+int main()
+{
+    int n, i, j;
+    printf("Enter number of Vertices : ");
+    scanf("%d", &n);
+    int arr[n][n];
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+            scanf("%d", &arr[i][j]);
+    }
+    printf("Graph :\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("%d ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}*/
+#include <stdio.h>
+
+int main()
+{
+    return 0;
+}
