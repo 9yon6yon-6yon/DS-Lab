3d38614bfb6443a67d7ad0c05208128956fdbb1b
Md. Asif Karim Chayon <mchayon201366@bscse.uiu.ac.bd>
2022-01-19 14:15:17 +0600

    GRAPH

--- Graph_12_1_22.c
+++ Graph_12_1_22.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
-
+// in a tree there will be no loop,direction and multiple edges
+// all trees are graph but all graphs are not tree
+/*void Visit(int g[100][100], int visited[], int n)
+{
+    printf("%c visited \n", 65 + n);
+    visited[n] = 1;
+}
+void dfs(int g[100][100], int visited[], int k, int n)
+{
+    int i;
+    Visit(g, visited, k);
+    for (i = 0; i < n; i++)
+    {
+        if (g[k][i] == 1 && visited[i] == 0)
+            dfs(g, visited, i, n);
+    }
+}
+*/
 int main()
 {
-    int n, i, j;
+    int n, i, j, visited[100];
     printf("Enter number of Vertices : ");
     scanf("%d", &n);
-    int arr[n + 1][n + 1];
+    int arr[n+1][n+1];
     for (i = 0; i < n; i++)
     {
         for (j = i + 1; j < n; j++)
@@ -24,6 +41,7 @@ int main()
         }
         printf("\n");
     }
+   // dfs(arr, visited, 0, n);
 
     return 0;
 }
